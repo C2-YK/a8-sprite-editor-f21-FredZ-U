ea204e69c9eb2e29e095d901189fde813db2ad67
FredZ-U <u1264317@utah.edu>
2021-11-16 15:32:57 -0700

    Revert "Merge branch 'main' into UIgangs"
    This reverts commit 77eccba999c2be8c8492c5ce38064c50d7e4899b, reversing
    changes made to 9b63f143e2cc31a38906c2be0906ad7bd83476f3.

--- filesystem.h
+++ filesystem.h
@@ -3,12 +3,10 @@
 
 #include <QObject>
 #include <QString>
+#include "sprite.h"
 #include <QFile>
 #include <QJsonDocument>
 #include <QJsonObject>
-#include <QJsonArray>
-#include <vector>
-#include "sprite.h"
 
 class FileSystem : public QObject
 {
@@ -24,9 +22,6 @@ signals:
     void saveCallback(bool success);
 private:
     Sprite* sprite;
-    void spriteWriter(QJsonObject &json);
-    void frameWriter(QJsonObject &json,const QImage &image);
-    std::vector<std::vector<std::vector<int>>> getColorMatrix(const QImage &image);
 };
 
 #endif // FILESYSTEM_H
