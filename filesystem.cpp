a4c92ca449a927e48219e7db7e3ef120657434cc
FredZ-U <u1264317@utah.edu>
2021-11-16 19:42:06 -0700

    Revert "Merge branch 'main' into UIgangs"
    This reverts commit 77eccba999c2be8c8492c5ce38064c50d7e4899b, reversing
    changes made to 9b63f143e2cc31a38906c2be0906ad7bd83476f3.

--- filesystem.cpp
+++ filesystem.cpp
@@ -31,47 +31,7 @@ void FileSystem::saveSprite(QString filename){
 
     QJsonObject spriteObject;
     //write method
-    spriteWriter(spriteObject);
     QJsonDocument saveDoc(spriteObject);
     saveFile.write(saveDoc.toJson());
-}
 
-void FileSystem:: spriteWriter(QJsonObject &json){
-
-    json["height"] = sprite->getHeight();
-    json["width"] = sprite->getWidth();
-    json["numberOfFrames"] = sprite->getMaxFrame();
-
-    QJsonArray frameArray;
-    foreach(Frame* frame, sprite->getFrames())
-    {
-        QJsonObject frameObject;
-        QImage target = frame->getImage();
-        frameWriter(frameObject,target);
-        frameArray.append(frameObject);
-    }
-    json["frames"] = frameArray;
-
-}
-
-void FileSystem:: frameWriter(QJsonObject &json, const QImage& image)
-{
-    std::vector<std::vector<std::vector<int>>> colorMatrix = getColorMatrix(image);
-    json["frame"] = QString::fromStdString(std::string(colorMatrix.begin(),colorMatrix.end()));
-}
-
-std::vector<std::vector<std::vector<int>>> FileSystem:: getColorMatrix(const QImage &image)
-{
-    int width =image.width();
-    int height = image.height();
-    std::vector<std::vector<std::vector<int>>> result;
-    for(int i = 0; i < width; i++)
-    {
-        for(int j =0; j < height; j++ )
-        {
-            std::vector<int> color = {image.pixelColor(i,j).alpha(),image.pixelColor(i,j).red(),image.pixelColor(i,j).green(),image.pixelColor(i,j).blue()};
-            result[i][j] = color;
-        }
-    }
-    return result;
 }
